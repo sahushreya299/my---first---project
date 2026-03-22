84e60f480e40f70ff39fd8842642a4c96ad09e95
sahushreya299 <sahushreya30196@gmail.com>
2026-03-22 17:19:01 +0530

    Create NumberCheck.java

---
+++ NumberCheck.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+      // Create a scanner for input (maps to READ) 
+      Scanner input = new Scanner(System.in);
+
+     // DECLARE num1 : INTEGER 
+     int num1;
+
+     System.out.println("Enter a number: ");
+     // READ num1
+     num1 = input.nextInt();
+
+    // condition Logic 
+    if (num1 > 0) {
+         // PRINT "Positive"
+         System.out.println("Negative"); 
+    }
+    else if {
+      // ELSE IF num < 0 THEN PRINT "Negative"
+      System.out.println("Negative");
+    }
+    else {
+      // ELSE PRINT "Zero"
+      System.out.println("Zero");
+    }
+
+    input.close();
+    }
+}
